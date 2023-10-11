6d0afba398c44ff3eb0b34a8e2ed77acaba47ff3
Nick <jeremy3773@mail.ru>
2023-10-12 00:00:25 +0300

    mikroorm

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
 import { Post } from './entities/Post'
+import mikroOrmConfig from './mikro-orm.config'
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        entities: [Post],
-        dbName: 'lireddit',
-        type: 'postgresql',
-        debug: !__prod__
-    })
+    const orm = await MikroORM.init(mikroOrmConfig)//инициализация MikroORM и подключение к базе данных
 
-    const post = orm.em.create(Post, {createAt: new Date(), title: 'my first post'})
+    const post = orm.em.create(Post, {createAt: new Date(), title: 'my first post'})//создаём экзепляр сущности Post
+    await orm.em.persistAndFlush(post)
+    console.log('------------------sql 2 --------------')
 }
-
-main()
+main().catch(err => console.log(err))
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,14 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+
+export default {
+    entities: [Post],//указываем, какие сущности будут управляться MikroORM
+    dbName: 'lireddit',//далее параметры подключения к базе данных
+    type: 'postgresql',
+    debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0]//тут следующая магия:
+//Parameters - встроенный typescript-тип, который позволяет извлекать типы
+//  параметров из функции. Это удобный способ получения типов аргументов,
+//  передаваемых в функцию.
+/MikroOrm.init() - эта функция может принимать 3 типа интерфейсов. Первый из них мне и нужен, поэтому обращаемся к нулевому индексу
