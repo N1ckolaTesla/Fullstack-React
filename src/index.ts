46892e3752c1f7fd317fa93478263c4675dd7622
Nick <jeremy3773@mail.ru>
2023-10-15 23:42:08 +0300

    fff

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import mikroOrmConfig from './mikro-orm.config'
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig)//инициализация MikroORM и подключение к базе данных
-
-    const post = orm.em.create(Post, {createAt: new Date(), title: 'my first post'})//создаём экзепляр сущности Post
+    const em = orm.em.fork() //создаём контексный EntityManager
+    const post = em.create(Post, {createAt: new Date(), title: 'my first post'})//создаём экзепляр сущности Post
     await orm.em.persistAndFlush(post)
     console.log('------------------sql 2 --------------')
 }
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,12 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
+import path from 'path'
 
 export default {
+    migrations: {
+        path: path.join(__dirname, './migrations')
+    },
     entities: [Post],//указываем, какие сущности будут управляться MikroORM
     dbName: 'lireddit',//далее параметры подключения к базе данных
     type: 'postgresql',
@@ -11,4 +15,4 @@ export default {
 //Parameters - встроенный typescript-тип, который позволяет извлекать типы
 //  параметров из функции. Это удобный способ получения типов аргументов,
 //  передаваемых в функцию.
-/MikroOrm.init() - эта функция может принимать 3 типа интерфейсов. Первый из них мне и нужен, поэтому обращаемся к нулевому индексу
+//MikroOrm.init() - эта функция может принимать 3 типа интерфейсов. Первый из них мне и нужен, поэтому обращаемся к нулевому индексу
